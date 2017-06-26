99320d11589be62f363dabfaa7a1daccb80f0835
Ethan Twardy <etwardy@xes-inc.com>
2017-06-26 16:01:02 -0500

    Some more changes

--- dbi.pl
+++ dbi.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl -w
 ################################################################################
 # NAME:		    dbi.pl
 #
@@ -9,3 +10,31 @@
 #
 # LAST EDITED:	    06/26/2017
 ###
+
+################################################################################
+# Includes
+###
+
+use strict;
+use warnings;
+use DBI;
+
+################################################################################
+# Main
+###
+
+my $un = "";
+my $pd = "";
+my $dbh = DBI->connect("dbi:SQLite:/home/etwardy/my_work/Language-Database/test.db", "$un", "$pd", { RaiseError => 1});
+
+my $sql = <<'EOF';
+CREATE TABLE test (
+    id	    INTEGER PRIMARY KEY,
+    name    VARCHAR(20)
+)
+EOF
+
+my $sth = $dbh->prepare($sql);
+$sth->execute();
+
+$dbh->disconnect();
