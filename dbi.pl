cd97b5beff925272b21167f0c6ace5adc2b95115
Ethan Twardy <etwardy@xes-inc.com>
2017-06-27 09:14:13 -0500

    Deleted stray file and finished basic SQLite CLI.

--- dbi.pl
+++ dbi.pl
@@ -4,7 +4,8 @@
 #
 # AUTHOR:	    Ethan D. Twardy
 #
-# DESCRIPTION:	    This perl script interfaces with the module in LanDB.pl
+# DESCRIPTION:	    This perl script creates a very simple command line tool
+#		    for using SQLite.
 #
 # CREATED:	    06/26/2017
 #
@@ -23,18 +24,68 @@ use DBI;
 # Main
 ###
 
-my $un = "";
-my $pd = "";
-my $dbh = DBI->connect("dbi:SQLite:/home/etwardy/my_work/Language-Database/test.db", "$un", "$pd", { RaiseError => 1});
+my $dbn = "dbi:SQLite";
+my $wd = $ENV{'PWD'};
 
-my $sql = <<'EOF';
-CREATE TABLE test (
-    id	    INTEGER PRIMARY KEY,
-    name    VARCHAR(20)
-)
-EOF
+print "Perl DBI Interface v1.0\n",
+    "***********************\n",
+    "What would you like to do?\n",
+    "Open File\n",
+    "Create File\n",
+    "Exit\n",
+    "***********************\n";
+my $ans = <STDIN>;
+$ans =~ s/([A-Z])/\L$1/;
+my $file;
+my $action;
+if ($ans =~ m/open/) {
 
-my $sth = $dbh->prepare($sql);
-$sth->execute();
+    do {
+	print "Please enter the name of a valid file you would like to open.\n";
+	$file = <STDIN>;
+	chomp $file
+    } while (!-e "$wd/$file");
+    $action = 'o';
 
+} elsif ($ans =~ m/create/) {
+
+    print "Please enter the name of a file you would like to create.\n";
+    $file = <STDIN>;
+    chomp $file;
+    $action = 'c';
+
+} elsif ($ans =~ m/exit/) {
+
+    exit 0;
+
+} else {
+
+    print "I didn't understand that command. Goodbye.\n";
+    exit 0;
+
+}
+
+my $dbh = DBI->connect("$dbn:$file", "", "", {
+    PrintError	=> 1,
+    RaiseError => 1,
+		       });
+print "Please enter SQLite commands, terminated by the string 'EOF'.\n",
+    "When you have finished, please enter exit.\n";
+
+{
+    local $/ = 'EOF';
+    while (1) {
+
+	my $sql = <STDIN>;
+	$sql =~ s/EOF//;
+	last if $sql =~ m/exit/;
+	my $sth = $dbh->prepare($sql);
+	$sth->execute();
+	print "Executed normally.\n"
+
+    }
+}
 $dbh->disconnect();
+print "Exiting.\n";
+
+################################################################################
